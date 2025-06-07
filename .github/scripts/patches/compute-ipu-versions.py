b2dd02b0b0b6bb5a7dfe87ccf02135f4af742b18
Michael J. Sullivan <sully@msully.net>
2025-06-06 17:19:25 -0700

    Test running inplace upgrades from instances created with 6.x (#8777)
    It uses the dump tests, since those should (should!) not change
    between versions.
    
    We'll test against 6.0 and 6.latest.
    Fixes #8776.

---
+++ .github/scripts/patches/compute-ipu-versions.py
@@ -0,0 +1,62 @@
+# Compute prior minor versions to test upgrading from
+
+
+import json
+import os
+import pathlib
+import re
+import sys
+from urllib import request
+
+sys.path.append(str(pathlib.Path(__file__).parent.parent.parent.parent))
+
+import edb.buildmeta
+
+base = 'https://packages.geldata.com'
+u = f'{base}/archive/.jsonindexes/x86_64-unknown-linux-gnu.json'
+data = json.loads(request.urlopen(u).read())
+
+u = f'{base}/archive/.jsonindexes/x86_64-unknown-linux-gnu.testing.json'
+data_testing = json.loads(request.urlopen(u).read())
+
+version = edb.buildmeta.EDGEDB_MAJOR_VERSION - 1
+
+versions = []
+prerelease_versions = []
+for obj in data['packages'] + data_testing['packages']:
+    if (
+        obj['basename'] == 'gel-server'
+        and obj['version_details']['major'] == version
+        and (
+            not obj['version_details']['prerelease']
+            or obj['version_details']['prerelease'][0]['phase'] in ('beta', 'rc')
+        )
+    ):
+        l = (
+            versions if not obj['version_details']['prerelease']
+            else prerelease_versions
+        )
+        l.append((
+            obj['version'],
+            obj['basename'],
+            base + obj['installrefs'][0]['ref'],
+        ))
+
+if not versions:
+    versions = prerelease_versions
+
+versions.sort(key=lambda x: x[0])
+if len(versions) > 1:
+    versions = [versions[0], versions[-1]]
+
+matrix = {
+    "include": [
+        {"edgedb-version": v, "edgedb-url": url, "edgedb-basename": base}
+        for v, base, url in versions
+    ]
+}
+
+print("matrix:", matrix)
+if output := os.getenv('GITHUB_OUTPUT'):
+    with open(output, 'a') as f:
+        print(f'matrix={json.dumps(matrix)}', file=f)
---
+++ tests/inplace-testing/test-old.sh
@@ -0,0 +1,148 @@
+#!/bin/bash -ex
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --rollback-and-test)
+        ROLLBACK=1
+        shift
+        ;;
+    --rollback-and-reapply)
+        REAPPLY=1
+        shift
+        ;;
+    --save-tarballs)
+        SAVE_TARBALLS=1
+        shift
+        ;;
+    *)
+        break
+        ;;
+  esac
+done
+
+
+DIR=$(realpath "$1")
+SERVER_INSTALL=$(realpath "$2")
+shift 2
+
+# Setup the test database
+(cd / && GEL_SERVER_SECURITY=insecure_dev_mode $SERVER_INSTALL/bin/python3  -m edb.tools --no-devmode inittestdb --tests-dir $SERVER_INSTALL/share/tests -k test_dump --data-dir "$DIR")
+
+
+if [ "$SAVE_TARBALLS" = 1 ]; then
+    tar cf "$DIR".tar "$DIR"
+fi
+
+
+PORT=12346
+GEL_SERVER_SECURITY=insecure_dev_mode $SERVER_INSTALL/bin/gel-server --testmode -D "$DIR" -P $PORT &
+SPID=$!
+stop_server() {
+    kill $SPID
+    wait $SPID
+    SPID=
+}
+cleanup() {
+    if [ -n "$SPID" ]; then
+        stop_server
+    fi
+}
+trap cleanup EXIT
+
+GEL="gel -H localhost -P $PORT --tls-security insecure --wait-until-available 120sec"
+
+# Wait for the server to come up and see it is working
+$GEL -b dump01 query 'select count(Z)' | grep 2
+
+# Block DDL
+$GEL query 'configure instance set force_database_error := $${"type": "AvailabilityError", "message": "DDL is disabled due to in-place upgrade.", "_scopes": ["ddl"]}$$;'
+
+if $GEL query 'create empty branch asdf'; then
+    echo Unexpected DDL success despite blocking it
+    exit 4
+fi
+
+# Prepare the upgrades
+EDGEDB_PORT=$PORT EDGEDB_CLIENT_TLS_SECURITY=insecure python3 tests/inplace-testing/prep-upgrades.py > "${DIR}/upgrade.json"
+
+# Get the DSN from the debug endpoint
+DSN=$(curl -s http://localhost:$PORT/server-info | jq -r '.pg_addr.dsn')
+
+# Prepare the upgrade, operating against the postgres that the old
+# version server is managing
+edb server --inplace-upgrade-prepare "$DIR"/upgrade.json --backend-dsn="$DSN"
+
+# Check the server is still working
+$GEL -b dump01 query 'select count(Z)' | grep 2
+
+if [ "$ROLLBACK" = 1 ]; then
+    # Inject a failure into our first attempt to rollback
+    if EDGEDB_UPGRADE_ROLLBACK_ERROR_INJECTION=dumpbasics edb server --inplace-upgrade-rollback --backend-dsn="$DSN"; then
+        echo Unexpected rollback success despite failure injection
+        exit 4
+    fi
+
+    # Second try should work
+    edb server --inplace-upgrade-rollback --backend-dsn="$DSN"
+    $GEL query 'configure instance reset force_database_error'
+
+    # XXX: what can we do here???
+    stop_server
+    # patch -R -f -p1 < tests/inplace-testing/upgrade.patch
+    # make parsers
+    # edb test --data-dir "$DIR" --use-data-dir-dbs -v "$@"
+    exit 0
+fi
+
+if [ "$REAPPLY" = 1 ]; then
+    # Rollback and then reapply
+    edb server --inplace-upgrade-rollback --backend-dsn="$DSN"
+
+    edb server --inplace-upgrade-prepare "$DIR"/upgrade.json --backend-dsn="$DSN"
+fi
+
+# Check the server is still working
+$GEL -b dump01 query 'select count(Z)' | grep 2
+
+# Kill the old version so we can finalize the upgrade
+stop_server
+
+if [ "$SAVE_TARBALLS" = 1 ]; then
+    tar cf "$DIR"-prepped.tar "$DIR"
+fi
+
+# Try to finalize the upgrade, but inject a failure
+if EDGEDB_UPGRADE_FINALIZE_ERROR_INJECTION=dumpbasics edb server --inplace-upgrade-finalize --data-dir "$DIR"; then
+    echo Unexpected upgrade success despite failure injection
+    exit 4
+fi
+
+# Try doing a rollback. It should fail, because of the partially
+# succesful finalization.
+if edb server --inplace-upgrade-rollback --data-dir "$DIR"; then
+    echo Unexpected upgrade success
+    exit 5
+fi
+
+# Finalize the upgrade
+edb server --inplace-upgrade-finalize --data-dir "$DIR"
+if [ "$SAVE_TARBALLS" = 1 ]; then
+    tar cf "$DIR"-cooked.tar "$DIR"
+fi
+
+# Start the server again so we can reenable DDL
+edb server -D "$DIR" -P $PORT &
+SPID=$!
+if $GEL query 'create empty branch asdf'; then
+    echo Unexpected DDL success despite blocking it
+    exit 6
+fi
+$GEL query 'configure instance reset force_database_error'
+stop_server
+if [ "$SAVE_TARBALLS" = 1 ]; then
+    tar cf "$DIR"-cooked2.tar "$DIR"
+fi
+
+
+# Test!
+edb test --data-dir "$DIR" --use-data-dir-dbs -v "$@" -k test_dump
