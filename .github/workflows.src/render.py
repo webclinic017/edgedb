991ce4bd59f81fbf2e256bd563f84ddd7e33a728
Elvis Pranskevichus <elvis@geldata.com>
2025-07-31 13:43:58 -0700

    workflows: Make language server use the common build template
    With a tad more configurability make `ls-build.inc.yml` redundant and
    just use common `build.inc.yml` to build all artifacts.  This also
    enables the testing step for the langserver target.
    
    For logistical reasons I had to rename the workflow files, but IMO the
    rename made things much cleaner, so worth it.

--- .github/workflows.src/render.py
+++ .github/workflows.src/render.py
@@ -22,8 +22,29 @@ def die(msg):
     sys.exit(1)
 
 
+def _expand_test_spec(target):
+    if "test" not in target:
+        target["test"] = {
+            "include": "",
+            "exclude": "",
+            "files": ""
+        }
+
+    for key in {"include", "exclude", "files"}:
+        if key not in target["test"]:
+            target["test"][key] = ""
+
+
+def _render(tpl_path, data):
+    with open(tpl_path) as f:
+        tpl = env.from_string(f.read())
+
+    return tpl.render(**data)
+
+
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--workflow', choices=["build", "test"], required=True)
     parser.add_argument('template')
     parser.add_argument('datafile')
 
@@ -35,9 +56,6 @@ def main():
     if not path.exists():
         die(f'template does not exist: {tplfile}')
 
-    with open(path) as f:
-        tpl = env.from_string(f.read())
-
     datapath = pathlib.Path(__file__).parent / args.datafile
 
     if datapath.exists():
@@ -46,7 +64,24 @@ def main():
     else:
         data = {}
 
-    output = tpl.render(**data)
+    if args.workflow == "build":
+        package = data.get("package")
+        if not package or not isinstance(package, dict):
+            die(f"invalid package: specification in {datapath}")
+        if not package.get("name"):
+            die(f"missing package.name in {datapath}")
+
+        _expand_test_spec(package)
+
+        targets = data.get("targets")
+        if not targets or not isinstance(targets, dict):
+            die(f"invalid targets: specification in {datapath}")
+
+        for target_list in targets.values():
+            for target in target_list:
+                _expand_test_spec(target)
+
+    output = _render(path, data)
 
     target = (
         pathlib.Path(__file__).parent.parent
